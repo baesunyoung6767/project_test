990fc21e3f9c3326f756d711a8e1b13c67b50d04
Sun Young Bae <tjsdud6677@gmail.com>
2022-12-27 11:51:04 +0900

    footer change

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -1,8 +1,27 @@
 import './Footer.css';
+import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+
+const Item = styled(Paper)(({ theme }) => ({
+    textAlign: 'center',
+    // backgroundColor:transparent
+}));
 
 function main() {
     return(
         <div className="footer">
+            <Grid container spacing={2}>
+                <Grid item xs={4}>
+                    <Item>xs=4</Item>
+                </Grid>
+                <Grid item xs={4}>
+                    <Item>xs=4</Item>
+                </Grid>
+                <Grid item xs={4}>
+                    <Item>xs=4</Item>
+                </Grid>
+            </Grid>
         </div>
     );
 }
