18886dd3c46ca72a6f977cd1dad604fd1c06c904
Sun Young Bae <tjsdud6677@gmail.com>
2022-12-28 11:49:42 +0900

    main page update

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -2,24 +2,40 @@ import './Footer.css';
 import Grid from '@mui/material/Grid';
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
+import { AiFillMail, AiOutlineGithub, AiFillPhone } from "react-icons/ai";
 
-const Item = styled(Paper)(({ theme }) => ({
+const Item1 = styled(Paper)(({ theme }) => ({
     textAlign: 'center',
-    // backgroundColor:transparent
+    color : 'white',
+    backgroundColor : 'black',
+    fontSize : '18px',
+    fontWeight : 'bold',
+    marginTop : '15px'
+}));
+
+const Item2 = styled(Paper)(({ theme }) => ({
+    textAlign: 'center',
+    color : 'white',
+    backgroundColor : 'black',
+    fontSize : '13px',
+    marginTop : '15px'
 }));
 
 function main() {
     return(
         <div className="footer">
-            <Grid container spacing={2}>
+            <Grid container spacing={3}>
                 <Grid item xs={4}>
-                    <Item>xs=4</Item>
+                    <Item1> <AiFillMail className='icon'/> email </Item1>
+                    <Item2> tjsdudqo1234@naver.com <br/> tjsdud6677@gmail.com </Item2>
                 </Grid>
                 <Grid item xs={4}>
-                    <Item>xs=4</Item>
+                    <Item1> <AiFillPhone className='icon'/> phone</Item1>
+                    <Item2> 010-8541-**** </Item2>
                 </Grid>
                 <Grid item xs={4}>
-                    <Item>xs=4</Item>
+                    <Item1> <AiOutlineGithub className='icon'/> github</Item1>
+                    <Item2> baesunyoung6767 </Item2>
                 </Grid>
             </Grid>
         </div>
--- src/component/main.js
+++ src/component/main.js
@@ -1,9 +1,51 @@
 import './css/main.css';
+import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
 
 function main() {
     return(
-        <div className="main">
-        </div>
+        <>
+            <div className="main1">
+                <div className="main1_item">
+                    <Grid container spacing={2}>
+                        <Grid item xs={5}>
+                            <div style={{marginRight:'30px', width : '300px'}}> 이미지 넣을 곳</div>
+                        </Grid>
+                        <Grid item xs={5}>
+                            <div style={{marginLeft:'30px', width : '500px'}}>
+                                <h2 className="main1_title">Hello!</h2>
+                                <h3>Bae Sun Young</h3>
+                                <p className="main1_item_content">
+                                    안녕하세요 저는 동의대학교 컴퓨터공학과 학생입니다 <br/>
+                                    웹 개발자를 목표로, 서버(node.js / springboot)와 클라이언트(react)를 공부하고 있습니다<br/>
+                                    저의 이력서와 포트폴리오가 궁금하시다면 아래 버튼을 클릭해주세요
+                                </p>
+                            </div>
+                        </Grid>
+                    </Grid>
+                </div>
+            </div>
+            <div className="main2">
+                <div className="main2_item">
+                    <Grid container spacing={3}>
+                        <Grid item xs={4}>
+                            <Button variant="contained" style={{backgroundColor: '#eafa3a', color: 'black'}}>RESUME</Button>
+                            <div className="main2_item_content">저의 이력서입니다<br/>공모전, 대외활동, 교내활동 등 확인하실 수 있습니다</div>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <Button variant="contained" style={{backgroundColor: '#eafa3a', color: 'black'}}>PROJECTS</Button>
+                            <div className="main2_item_content">지금까지 진행했던 프로젝트입니다</div>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <Button variant="contained" style={{backgroundColor: '#eafa3a', color: 'black'}}>CONTACT</Button>
+                            <div className="main2_item_content">궁금하신 점이 있다면 해당 페이지에서<br/>이메일을 통해 연락주세요</div>
+                        </Grid>
+                    </Grid>
+                </div>
+            </div>
+        </>
     );
 }
 
